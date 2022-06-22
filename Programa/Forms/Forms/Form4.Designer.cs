f4228b3fccf8c89eecf7e301274f96c121535f6e
David Raposo <david.raposo06@ua.pt>
2022-06-22 19:19:48 +0100

    +

---
+++ Programa/Forms/Forms/Form4.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace TripPlanner
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(23, 143);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(280, 284);
+            this.listBox1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(66, 38);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(185, 28);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form4";
+            this.Text = "Form4";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private ComboBox comboBox1;
+    }
+}
---
+++ Programa/Forms/Forms/Form4.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace TripPlanner
+{
+    public partial class Form4 : Form
+    {
+        private int currentCity;
+        private SqlConnection con;
+
+        public Form4()
+        {
+            InitializeComponent();
+            Load += new EventHandler(Form4_Load);
+            listBox1.DoubleClick += new EventHandler(ListBox1_DoubleClick);
+        }
+
+        private SqlConnection getSGBDConnection()
+        {
+            return new SqlConnection("Data Source=tcp:mednat.ieeta.pt\\SQLSERVER,8101;Initial Catalog=p1g1;Persist Security Info=True;User ID=p1g1;Password=Porto>Benfica;");
+        }
+
+        private bool verifySGBDConnection()
+        {
+            if (con == null)
+                con = getSGBDConnection();
+
+            if (con.State != ConnectionState.Open)
+                con.Open();
+
+            return con.State == ConnectionState.Open;
+        }
+
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            con = getSGBDConnection();
+            if (!verifySGBDConnection())
+            {
+                return;
+            }
+
+            SqlCommand cmd = new SqlCommand("SELECT CName, Country, Continent FROM TripPlanner.City", con);
+            SqlDataReader reader = cmd.ExecuteReader();
+            listBox1.Items.Clear();
+
+            while (reader.Read())
+            {
+                City C = new City();
+                C.City_name = reader["CName"].ToString();
+                C.Country_name = reader["Country"].ToString();
+                C.Continent_name = reader["Continent"].ToString();
+                listBox1.Items.Add(C.City_name);
+            }
+
+            con.Close();
+            //listBox1.SelectedIndex = 0;
+
+            currentCity = 0;
+           
+        }
+        private void ListBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                MessageBox.Show(listBox1.SelectedItem.ToString());
+            }
+        }
+
+
+        public void ShowCity()
+        {
+            if (listBox1.Items.Count == 0 | currentCity < 0)
+                return;
+            City city = new City();
+            city = (City)listBox1.Items[currentCity];
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+}
